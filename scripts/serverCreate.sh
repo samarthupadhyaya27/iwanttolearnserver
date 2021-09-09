77b6a2b5e949d315522b01cc49ca7d62d69afd29
Samarth Upadhyaya <samarthupadhyaya@Samarths-MacBook-Pro.local>
2021-09-10 04:15:36 +0800

    Injected first sql script into shell script

--- scripts/serverCreate.sh
+++ scripts/serverCreate.sh
@@ -1,4 +1,10 @@
 echo "Deleting existing database if it exists"
+# Delete database files if it already exists
 eval "rm -rf Postgres/data"
 eval "initdb -D Postgres/data"
+
+# Start the database
 eval "pg_ctl -D Postgres/data -l logfile start"
+
+if [ -f "scripts/schemaSetupFiles/createDatabase.sql" ]; then
+	eval psql -p 5432 -d postgres -f "scripts/schemaSetupFiles/createDatabase.sql"
