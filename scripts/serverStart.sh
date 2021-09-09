1e94d92fd0e438e2cefef85d5b34f22098651c22
Samarth Upadhyaya <samarthupadhyaya@Samarths-MacBook-Pro.local>
2021-09-10 03:36:21 +0800

    More database setup script pushes

--- scripts/serverStart.sh
+++ scripts/serverStart.sh
@@ -3,9 +3,9 @@
 echo "Starting a PostgreSQL server in the database/data database cluster on port 5444"
 # We first need to kill any running processes on port 5444
 if [[ $OSTYPE == 'darwin'* ]]; then
-	eval lsof -ti tcp:5444 | xargs kill -9
+	eval lsof -ti tcp:5432 | xargs kill -9
 else
-	eval lsof -ti tcp:5444 | xargs --no-run-if-empty kill -9
+	eval lsof -ti tcp:5432 | xargs --no-run-if-empty kill -9
 fi
 
-eval "pg_ctl start -l Postgres/postgresql.log -o \"-F -p 5444\" -D Postgres/data"
+eval "pg_ctl start -l Postgres/postgresql.log -o \"-F -p 5432\" -D Postgres/data"
