db41f22ecce72819d87e682cb9608770c67540b4
Victor Wen <vicwen02@g.ucla.edu>
2021-09-20 20:22:16 -0700

    Added table and testing values for roadmap

--- scripts/schemaSetupFiles/createTables.sql
+++ scripts/schemaSetupFiles/createTables.sql
@@ -2,9 +2,19 @@ DO $$ BEGIN RAISE NOTICE 'Creating tables'; END $$;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users(
-	user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email varchar(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roadmaps(
+    map_Id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    author_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_author
+        FOREIGN KEY(author_id) 
+            REFERENCES users(user_id)
+            ON DELETE CASCADE
 );
--- scripts/schemaSetupFiles/initializeDatabase.sql
+++ scripts/schemaSetupFiles/initializeDatabase.sql
@@ -1,4 +1,10 @@
 DO $$ BEGIN RAISE NOTICE 'Initializing users'; END $$;
 
 INSERT INTO users (first_name, last_name, email, password ) values
-    ('Samarth', 'Upadhyaya', 'samarthupadhyaya27@gmail.com', 'testing');
+    ('Samarth', 'Upadhyaya', 'samarthupadhyaya27@gmail.com', 'testing'),
+    ('Victor', 'Wen', 'vicwen02@g.ucla.edu', 'superstrongpasswordhash');
+
+INSERT INTO roadmaps (author_id, title) VALUES
+    ((SELECT user_id FROM users WHERE first_name = 'Victor'), 'Beginner to Pro with PostgreSQL'),
+    ((SELECT user_id FROM users WHERE first_name = 'Victor'), 'Typing Typescript'),
+    ((SELECT user_id FROM users WHERE first_name = 'Victor'), 'NodeJS for advanced programmers');
